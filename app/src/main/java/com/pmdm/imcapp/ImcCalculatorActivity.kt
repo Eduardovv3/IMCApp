be30264262f39b20957cf80e93ed51518b070cb2
Eduardo <Hola@gmail.com>
2023-11-14 13:13:11 +0100

    VERSION 2

--- app/src/main/java/com/pmdm/imcapp/ImcCalculatorActivity.kt
+++ app/src/main/java/com/pmdm/imcapp/ImcCalculatorActivity.kt
@@ -2,10 +2,24 @@ package com.pmdm.imcapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.cardview.widget.CardView
 
 class ImcCalculatorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc_calculator)
+        initiListeners()
     }
+    private lateinit var viewMale: CardView
+    private lateinit var viewFemale:CardView
+    private fun initComponents() {
+        viewMale = findViewById(R.id.viewMale)
+        viewFemale = findViewById(R.id.viewFemale)
+    }
+    private fun initListeners(){
+        viewMale.setOnClickListener()
+        viewFemale.setOnClickListener()
+
+    }
+
 }
